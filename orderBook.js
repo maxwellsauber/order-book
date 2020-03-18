673e15b609a39205c75b67e1fef1b5e65c8d0e0e
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-18 16:43:32 -0400

    For review with TA

--- orderBook.js
+++ orderBook.js
@@ -1,20 +1,37 @@
 const reconcileOrder = (existingBook, incomingOrder) => {
-  if (existingBook.length < 1) { // Empty Existing Book
+
+  if (existingBook.length < 1) { // Empty Existing Book... test 1
     return [incomingOrder]
   }
 
-  for (let i = 0; existingBook.length; i++) {
-    if (existingBook[i].type == incomingOrder.type || existingBook[0].price != incomingOrder.price) {
-      // If both books have same type OR if prices do not match
-      const returnBook = []
+  const returnBook = []
 
-      returnBook.push(existingBook[0])
+  for (let i = 0; existingBook.length; i++) {
+
+
+    if (existingBook[i].type === incomingOrder.type || existingBook[i].price !== incomingOrder.price) {
+      // If both books have same type (test 2) OR if prices do not match (test 3)
+
+      returnBook.push(existingBook[i])
       returnBook.push(incomingOrder)
+
+      return returnBook // WHY DOES THHIS NOT WORK OUTSIDE IF/FOR Statement????? returns first item
+    }
+
+    /*
+    else if (existingBook[i].price === incomingOrder.price && existingBook[i].price === incomingOrder.price) {
+
+      returnBook.push(existingBook[i])
 
       return returnBook
     }
+    */
+
 
   }
+
+  //return returnBook //This should work out here?
+
 
 }
 
