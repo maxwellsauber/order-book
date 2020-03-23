b60d88c93fde5b893cd5d4ee4a59ffde6dd77d73
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-23 13:55:15 -0400

    TESTS PASS

--- orderBook.js
+++ orderBook.js
@@ -1,35 +1,54 @@
-const reconcileOrder = (existingBook, incomingOrder) => {
+const reconcileOrder = (existingBook, incomingOrder) =>
+  existingBook.length
+    ? checkType(existingBook, incomingOrder)
+    : addToBook(existingBook, incomingOrder)
 
-  if (existingBook.length < 1) { // Empty Existing Book... test 1
-    return [incomingOrder]
-  }
+const checkType = (existing, incoming) =>
+  existing.every((obj) => obj.type === incoming.type)
+    ? addToBook(existing, incoming)
+    : checkPrice(existing, incoming)
 
-  const returnBook = []
-
-  for (let i = 0; i < existingBook.length; i++) {
-
-
-    if (existingBook[i].type === incomingOrder.type || existingBook[i].price !== incomingOrder.price) {
-      // If both books have same type (test 2) OR if prices do not match (test 3)
-
-      returnBook.push(existingBook[i])
-      returnBook.push(incomingOrder)
-
-      //return returnBook // WHY DOES THHIS NOT WORK OUTSIDE IF/FOR Statement????? returns first item
+const checkPrice = (existing, incoming) => {
+  for (let i = 0; i < existing.length; i++) {
+    if (existing[i].price === incoming.price) {
+      return checkQuantity(existing, incoming)
     }
-    else if (existingBook[i].price === incomingOrder.price && existingBook[i].price === incomingOrder.price) {
-
-      returnBook.push(existingBook[i])
-
-    }
-
-
-
   }
-
-  return returnBook //This should work out here?
-
-
+  return addToBook(existing, incoming)
 }
+
+const checkQuantity = (existing, incoming) => {
+  for (let i = 0; i < existing.length; i++) {
+    if (existing[i].quantity === incoming.quantity) {
+      existing.splice(i, 1)
+      return existing
+    } else if (existing[i].quantity > incoming.quantity) {
+      existing[i].quantity -= incoming.quantity
+      return existing
+    } else if (existing[i].quantity < incoming.quantity) {
+      incoming.quantity -= existing[i].quantity
+      existing.splice(i, 1)
+      //existing.push(incoming)
+      return checkType(existing, incoming) //Recursion fun time
+    }
+  }
+  // return addToBook(existing, incoming)
+}
+
+
+const addToBook = (existing, incoming) => {
+  existing.push(incoming)
+  return existing
+}
+
+/*
+
+const equalPrices = (incoming, existing) => { }
+const equalQuantities = (incoming, existing) => { }
+const unequalPrices = (incoming, existing) => { }
+const unequalQuantities = (incoming, existing) => { }
+*/
+
+
 
 module.exports = reconcileOrder
